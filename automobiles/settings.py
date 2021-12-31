2bbd9b2f1b91c2e3d53b087b1de1b450c91eb970
hacker6125 <shagunsharmma@gmail.com>
2021-12-31 17:59:16 +0530

    Version 1.0.2

--- automobiles/settings.py
+++ automobiles/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7^m9_*)vimdvm0kf0_@r+jza^&awr&x1-zl(1+g897opls$s1l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'armygateautomobiles.heroku.com']
 
 
 # Application definition
@@ -129,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
